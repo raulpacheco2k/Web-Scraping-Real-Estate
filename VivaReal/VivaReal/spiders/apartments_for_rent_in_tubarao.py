82ed943e08b72618f58a6aa5e9dfcf2f7d41c699
Raul Pacheco <eu@raulpacheco.com.br>
2021-10-23 20:46:31 -0300

    Adiciona tratamento de dados

--- VivaReal/VivaReal/spiders/apartments_for_rent_in_tubarao.py
+++ VivaReal/VivaReal/spiders/apartments_for_rent_in_tubarao.py
@@ -1,4 +1,11 @@
+import re
+from unidecode import unidecode
+
 import scrapy
+
+
+def sanitize(letras):
+    return re.sub('[^A-Za-z0-9_ ]+', '', unidecode(letras))
 
 
 class ApartmentsForRentInTubarao(scrapy.Spider):
@@ -8,7 +15,7 @@ class ApartmentsForRentInTubarao(scrapy.Spider):
     def _parse(self, response, **kwargs):
         for aparments in response.css('a.js-card-title'):
             yield {
-                'title': aparments.css('div h2 span::text')[0].get().strip(),
-                'address': aparments.css('div h2 span')[1].css('span::text')[1].get().strip(),
-                'squareMeter': aparments.css('div h2 span')[1].css('span::text')[1].get().strip(),
+                'title': sanitize(aparments.css('div h2 span::text')[0].get().strip()),
+                'address': sanitize(aparments.css('div h2 span')[1].css('span::text')[1].get().strip()),
+                'squareMeter': sanitize(aparments.css('div h2 span')[1].css('span::text')[1].get().strip()),
             }
