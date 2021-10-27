c93939c199f669c8215aaa6c6eecf41a51428a7d
Raul Pacheco <eu@raulpacheco.com.br>
2021-10-27 13:00:46 -0300

    Melhora nomeclatura das variáveis

--- VivaReal/VivaReal/spiders/apartments_for_rent_in_tubarao.py
+++ VivaReal/VivaReal/spiders/apartments_for_rent_in_tubarao.py
@@ -1,6 +1,4 @@
 import re
-from unidecode import unidecode
-
 import scrapy
 import unicodedata
 
@@ -19,14 +17,15 @@ class ApartmentsForRentInTubarao(scrapy.Spider):
     start_urls = ['https://www.vivareal.com.br/aluguel/santa-catarina/tubarao/apartamento_residencial/']
 
     def parse(self, response, **kwargs):
-        for apartments in response.css('a.js-card-title'):
+        apartments = response.css('a.js-card-title')
+        for apartment in apartments:
             yield {
-                'title': sanitize(apartments.css('div h2 span::text')[0].get()),
-                'price': sanitize(apartments.css('div section div p::text').get()).replace('R ', ''),
-                'period': sanitize(apartments.css('div section div p span::text').get()).replace('/', ''),
-                'address': sanitize(apartments.css('div h2 span')[1].css('span::text')[1].get()),
-                'squareMeter': sanitize(apartments.css('div ul')[0].css('li')[0].css('span::text')[0].get()),
-                'bedrooms': sanitize(apartments.css('div ul')[0].css('li')[1].css('span::text')[0].get()),
-                'bathroom': sanitize(apartments.css('div ul')[0].css('li')[2].css('span::text')[0].get()),
-                'parkingSpaces': sanitize(apartments.css('div ul')[0].css('li')[3].css('span::text')[0].get())
+                'title': sanitize(apartment.css('div h2 span::text')[0].get()),
+                'price': sanitize(apartment.css('div section div p::text').get()).replace('R ', ''),
+                'period': sanitize(apartment.css('div section div p span::text').get()).replace('/', ''),
+                'address': sanitize(apartment.css('div h2 span')[1].css('span::text')[1].get()),
+                'squareMeter': sanitize(apartment.css('div ul')[0].css('li')[0].css('span::text')[0].get()),
+                'bedrooms': sanitize(apartment.css('div ul')[0].css('li')[1].css('span::text')[0].get()),
+                'bathroom': sanitize(apartment.css('div ul')[0].css('li')[2].css('span::text')[0].get()),
+                'parkingSpaces': sanitize(apartment.css('div ul')[0].css('li')[3].css('span::text')[0].get())
             }
