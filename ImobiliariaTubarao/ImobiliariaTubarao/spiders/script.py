d0d266810c388efb575320a85ceaa07aafacc48d
Raul Pacheco <eu@raulpacheco.com.br>
2021-10-28 21:26:14 -0300

    Adiciona botão de seguir a página

--- ImobiliariaTubarao/ImobiliariaTubarao/spiders/script.py
+++ ImobiliariaTubarao/ImobiliariaTubarao/spiders/script.py
@@ -23,7 +23,6 @@ class ImobiliariaTubarao(scrapy.Spider):
         properties = response.css('.imovel_ideal_home')
 
         for property in properties:
-
             datails = property
             property = property.css('.trans')
 
@@ -35,6 +34,13 @@ class ImobiliariaTubarao(scrapy.Spider):
                 'squareMeter': Sanitize.clean(property.css('ul li::text')[3].get().strip().split(' ')[0]),
                 'bedrooms': Sanitize.clean(property.css('ul li::text')[0].get().strip().split(' ')[0]),
                 'bathroom': Sanitize.clean(property.css('ul li::text')[1].get().strip().split(' ')[0]),
-                'parkingSpaces': Sanitize.clean(property.css('div').css('ul').css('li::text')[2].get().strip().split(' ')[0]),
+                'parkingSpaces': Sanitize.clean(
+                    property.css('div').css('ul').css('li::text')[2].get().strip().split(' ')[0]),
                 'link': self.allowed_domains[0] + datails.css('a::attr(href)')[1].get()
             }
+
+        try:
+            next_page = response.css('#accordion nav ul li')[3].css('a::attr(href)').get()
+            yield response.follow(url=next_page, callback=self.parse)
+        except ValueError:
+            self.logger.info('Não há mais páginas.')
